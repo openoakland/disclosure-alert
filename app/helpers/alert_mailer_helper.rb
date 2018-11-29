12f9d12554daea9365c3f196e896a38491185830
Tom Dooner <tomdooner@gmail.com>
2018-11-29 12:40:22 -0800

    Fix HTML formatting of form name
    After converting to Rails, the form name needed to be marked HTML Safe,
    or else forms with names like '497 LCR' would render as '497
    <sub>LCR</sub>'.
    
    By extracting the logic to a helper, it should also be more testable.

--- app/helpers/alert_mailer_helper.rb
+++ app/helpers/alert_mailer_helper.rb
@@ -2,4 +2,16 @@ module AlertMailerHelper
   def format_money(amount)
     '$' + amount.round.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
   end
+
+  def form_number_html(form)
+    if form.form_name
+      sanitize(form.form_name)
+        .gsub(/([[:alpha:]]+)/i, '<sub>\1</sub>')
+        .html_safe
+    elsif form.title =~ /FPPC Form (\d+)/
+      $~[1]
+    else
+      '???'
+    end
+  end
 end
