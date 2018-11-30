2aea53a1c76847daa018f3bd485f17547c2f8180
Tom Dooner <tomdooner@gmail.com>
2018-11-30 13:42:16 -0800

    Generate a token automatically for AlertSubscriber objects

--- app/models/alert_subscriber.rb
+++ app/models/alert_subscriber.rb
@@ -2,4 +2,12 @@
 
 class AlertSubscriber < ApplicationRecord
   validates :email, format: /\A[^@]+@[^\.]+\.[\w]+\z/i
+
+  before_save :set_token_if_missing
+
+  def set_token_if_missing
+    return if token.present?
+
+    self[:token] = SecureRandom.hex
+  end
 end
