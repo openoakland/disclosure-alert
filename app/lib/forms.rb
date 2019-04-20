fbd915ac061bee91011fcdd1ce8e4f6f7eb9f539
Tom Dooner <tomdooner@gmail.com>
2019-04-20 13:41:58 -0700

    Add human-friendly descriptions of forms
    Although the email is targeted at experts right now, let's see if we can
    make it a bit more inclusive without adding too much extra content.
    
    This commit adds section headings for each type of form, groups the
    forms by type, and, at the beginning of the section heading, includes a
    bit of introductory material to the form.
    
    Maybe, eventually, we can actually keep track of the state of onboarding
    for each user, and skip the descriptions if the user's seen it a million
    times already.

--- app/lib/forms.rb
+++ app/lib/forms.rb
@@ -30,6 +30,13 @@ module Forms
     # @retun {Hash?} Returns a hash with keys position, agency, and
     # division_board_district.
     def filer_title; end
+
+    # I18n key to describe the Form
+    def i18n_key
+      return "forms.unknown" unless @filing.form_name.present?
+
+      "forms.#{@filing.form_name.delete(' ')}"
+    end
   end
 
   class BaseXMLForm < BaseForm
