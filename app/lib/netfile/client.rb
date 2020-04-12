52af7edab13d9ca47195a825f67391a2de5afcaa
Tom Dooner <tomdooner@gmail.com>
2020-04-11 20:01:43 -0700

    Add pagination to Netfile::Client
    We'll sometimes need to download more than 200 records. Pagination
    already existed for the filing list, this extracts it into a helper
    method.

--- app/lib/netfile/client.rb
+++ app/lib/netfile/client.rb
@@ -14,39 +14,38 @@ module Netfile
 
     def fetch_summary_contents(filing_id)
       Net::HTTP.start(BASE_URL.host, BASE_URL.port, use_ssl: true) do |http|
-        request = Net::HTTP::Post.new(BASE_URL + 'public/campaign/export/cal201/summary/filing')
-        request['Accept'] = 'application/json'
-        request.body = URI.encode_www_form(
-          FilingId: filing_id,
-          PageSize: 200,
-        )
+        with_pagination do |current_page|
+          request = Net::HTTP::Post.new(BASE_URL + 'public/campaign/export/cal201/summary/filing')
+          request['Accept'] = 'application/json'
+          request.body = URI.encode_www_form(
+            FilingId: filing_id,
+            PageSize: 200,
+          )
 
-        response = http.request(request)
-        raise "Request Failed: " + request.inspect unless response.code.to_i < 300
+          response = http.request(request)
+          raise "Request Failed: " + request.inspect unless response.code.to_i < 300
 
-        results, num_pages = JSON.parse(response.body).values_at('results', 'totalMatchingPages')
-        raise "Error: More than one page would be returned. Bump PageSize." if num_pages > 1
-
-        results
+          JSON.parse(response.body).values_at('results', 'totalMatchingPages')
+        end
       end
     end
 
     def fetch_transaction_contents(filing_id)
       Net::HTTP.start(BASE_URL.host, BASE_URL.port, use_ssl: true) do |http|
-        request = Net::HTTP::Post.new(BASE_URL + 'public/campaign/export/cal201/transaction/filing')
-        request['Accept'] = 'application/json'
-        request.body = URI.encode_www_form(
-          FilingId: filing_id,
-          PageSize: 200,
-        )
+        with_pagination do |current_page|
+          request = Net::HTTP::Post.new(BASE_URL + 'public/campaign/export/cal201/transaction/filing')
+          request['Accept'] = 'application/json'
+          request.body = URI.encode_www_form(
+            FilingId: filing_id,
+            CurrentPageIndex: current_page,
+            PageSize: 200,
+          )
 
-        response = http.request(request)
-        raise "Request Failed: " + request.inspect unless response.code.to_i < 300
+          response = http.request(request)
+          raise "Request Failed: " + request.inspect unless response.code.to_i < 300
 
-        results, num_pages = JSON.parse(response.body).values_at('results', 'totalMatchingPages')
-        raise "Error: More than one page would be returned. Bump PageSize." if num_pages > 1
-
-        results
+          JSON.parse(response.body).values_at('results', 'totalMatchingPages')
+        end
       end
     end
 
@@ -86,10 +85,8 @@ module Netfile
     def each_filing(form: nil, &block)
       return to_enum(:each_filing) unless block_given?
 
-      current_page = 0
-
       Net::HTTP.start(BASE_URL.host, BASE_URL.port, use_ssl: true) do |http|
-        loop do
+        with_pagination do |current_page|
           request = Net::HTTP::Post.new(BASE_URL + 'public/list/filing')
           request['Accept'] = 'application/json'
           request.body = URI.encode_www_form(
@@ -101,19 +98,30 @@ module Netfile
           response = http.request(request)
           raise 'Error: ' + response.inspect unless response.code.to_i < 300
 
-          filings, total_count =
-            JSON.parse(response.body).values_at('filings', 'totalMatchingCount')
-
-          remaining_count ||= total_count
-          remaining_count -= filings.length
+          filings, num_pages =
+            JSON.parse(response.body).values_at('filings', 'totalMatchingPages')
 
           filings.each(&block)
 
-          break if remaining_count <= 0
-
-          current_page += 1
+          [nil, num_pages]
         end
       end
+    end
+
+    private
+
+    def with_pagination
+      current_page = 0
+      results = []
+
+      loop do
+        page_results, num_pages = yield current_page
+        results.concat(page_results)
+        break if num_pages == current_page
+        current_page += 1
+      end
+
+      results
     end
   end
 end
