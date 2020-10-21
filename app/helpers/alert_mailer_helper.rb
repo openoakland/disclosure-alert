428a7c5700d658ef1d9639bcc7e992f575958a5e
Tom Dooner <tomdooner@gmail.com>
2020-10-20 21:00:58 -0700

    DRY up logic for listing top 3 contributions/expenditures
    I want to make this logic a bit more complicated.

--- app/helpers/alert_mailer_helper.rb
+++ app/helpers/alert_mailer_helper.rb
@@ -5,6 +5,25 @@ module AlertMailerHelper
     '$' + amount.round.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
   end
 
+  def format_money_list(items, key:, extra: '', &block)
+    sorted_items = items.sort_by { |item| item[key] }.reverse
+    listed_items = safe_join(sorted_items[0..2].map do |item|
+      content_tag('li') do
+        money = item[key]
+        raise "Key #{key} not found in money list item" unless money.present?
+        format_money(money) + ' – ' + capture_haml(item, &block)
+      end
+    end, "\n")
+
+    remaining_item_count = sorted_items.length >= 3 ? sorted_items[3..-1].count : 0
+    if remaining_item_count > 0
+      remaining_item_amount = sorted_items[3..-1].sum { |item| item['tran_Amt1'] || item['calculated_Amount'] }
+      listed_items + content_tag('li', "and #{format_money(remaining_item_amount)} in #{format(extra, count: remaining_item_count)}")
+    else
+      listed_items
+    end
+  end
+
   def form_number_html(form)
     if form.form_name
       sanitize(form.form_name)
--- app/lib/forms/form497.rb
+++ app/lib/forms/form497.rb
@@ -4,22 +4,15 @@ module Forms
   # Form 497: Late Contributions Received/Made
   class Form497 < BaseForm
     def total_amount
-      contents.sum { |r| r['calculated_Amount'] }
+      contributions.sum { |r| r['calculated_Amount'] }
     end
 
     def contribution_count
-      contents.length
+      contributions.length
     end
 
-    def largest_three
+    def contributions
       contents
-        .sort_by { |i| i['calculated_Amount'] }
-        .reverse
-        .first(3)
-    end
-
-    def largest_three_total_amount
-      largest_three.map { |i| i['calculated_Amount'] }.sum
     end
   end
 end
