227cc194da19fd2e6f2749eeda705713090ea3b5
Tom Dooner <tomdooner@gmail.com>
2022-10-09 14:18:08 -0700

    Upgrade Rails 6.1 -> 7.0

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,27 +11,25 @@ module DisclosureAlert
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
-    config.autoload_paths << Rails.root.join('app', 'lib')
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    config.time_zone = "Pacific Time (US & Canada)"
+    config.eager_load_paths << Rails.root.join("app", "lib")
 
-    # Configure for sending email:
     config.action_mailer.delivery_method = :mailgun
     config.action_mailer.mailgun_settings = {
       api_key: ENV['MAILGUN_API_KEY'],
       domain: 'mailgun.opendisclosure.io',
     }
+
     config.action_mailer.default_url_options = {
       host: ENV['APP_HOST'],
     }
     config.action_mailer.preview_path = Rails.root.join('spec', 'mailers', 'previews')
 
     config.assets.precompile << 'email.scss'
-
-
-    config.time_zone = 'Pacific Time (US & Canada)'
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
   end
 end
