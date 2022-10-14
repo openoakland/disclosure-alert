d1666f33b056a16cc15c4b5325a03a9db0d83339
Tom Dooner <tomdooner@gmail.com>
2022-10-14 14:54:51 -0700

    Improve admin management of agency/subscriptions

--- app/admin/notices.rb
+++ app/admin/notices.rb
@@ -13,8 +13,9 @@ ActiveAdmin.register Notice do
     f.semantic_errors
     f.inputs do
       f.input :date, as: :datepicker, datepicker_options: {
-        min_date: Date.today,
+        min_date: Date.today - 1,
       }, input_html: { autocomplete: 'off' }
+      text_node "Note: The date should be the date of the *filings*, not the date of the email. (i.e. this date should match the subject line of the email)"
       f.input :body
     end
     f.actions
