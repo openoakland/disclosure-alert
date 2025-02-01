630b1b23b9356a4e5156110d694d6981a0a07165
Tom Dooner <tomdooner@gmail.com>
2025-01-31 21:23:45 -0800

    Remove deadlines from "subscription confirmed" email
    These deadlines were always out-of-date. I'll just replace them with the
    FPPC page that lists them, at least until there's another election
    season.

--- spec/mailers/previews/alert_subscriber_preview.rb
+++ spec/mailers/previews/alert_subscriber_preview.rb
@@ -1,6 +1,6 @@
 class AlertSubscriberPreview < ActionMailer::Preview
   def subscription_created
-    AlertSubscriberMailer.subscription_created(
+    AlertSubscriberMailer.subscription_confirmed(
       AlertSubscriber.first
     )
   end
