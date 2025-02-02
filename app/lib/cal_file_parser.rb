2ffc932f5a707c9ecabba90ce1059e71abbf27b6
Tom Dooner <tomdooner@gmail.com>
2025-02-02 14:14:49 -0800

    Add basic parsing for F496
    We'll need to parse the CVR sheet because it contains data about who the
    contributions and expenditures were supporting/opposing.

--- app/lib/cal_file_parser.rb
+++ app/lib/cal_file_parser.rb
@@ -5,6 +5,28 @@
 # This parser only supports:
 #   * Nothing yet, but it will eventually (hopefully)
 class CalFileParser
+  CVR_HEADERS = %w[filer_ID entity_Cd filer_NamL filer_NamF filer_NamT
+  filer_NamS report_Num rpt_Date stmt_Type rpt_ID_Num from_Date thru_Date
+  elect_Date filer_Adr1 filer_Adr2 filer_City filer_ST filer_ZIP4 filer_Phon
+  filer_FAX file_Email mail_Adr1 mail_Adr2 mail_City mail_ST mail_ZIP4 tres_NamL
+  tres_NamF tres_NamT tres_NamS tres_Adr1 tres_Adr2 tres_City tres_ST tres_ZIP4
+  tres_Phon tres_FAX tres_Email cmtte_Type] +
+  # See pg 26 of docs for what these flags mean (different things for different
+  # types of committee)
+  %w[flag1_YN flag2_YN flag3_YN flag4_YN] +
+  # 460, 465, 496
+  %w[amendExp_1 amendExp_2 amendExp_3 cand_NamL cand_NamF cand_NamT cand_NamS cand_Adr1 cand_Adr2 cand_City cand_ST cand_ZIP4 cand_Phon cand_FAX cand_Email bal_Name bal_Num bal_Juris office_Cd offic_Dscr juris_Cd juris_Dscr dist_No off_S_H_Cd sup_Opp_Cd]
+
+  RCPT_HEADERS = %w[tran_ID entity_Cd ctrib_NamL ctrib_NamF ctrib_NamT
+  ctrib_NamS ctrib_Adr1 ctrib_Adr2 ctrib_City ctrib_ST ctrib_ZIP4 ctrib_Emp
+  ctrib_Occ ctrib_Self tran_Type rcpt_Date date_Thru amount cum_YTD hold_Amount
+  12 ctrib_Dscr cmte_ID tres_NamL tres_NamF tres_NamT tres_NamS tres_Adr1
+  tres_Adr2 tres_City tres_ST tres_ZIP4 intr_NamL intr_NamF intr_NamT intr_NamS
+  intr_Adr1 intr_Adr2 intr_City intr_ST intr_ZIP4 intr_Emp intr_Occ intr_Self
+  cand_NamL cand_NamF cand_NamT cand_NamS office_Cd offic_Dscr juris_Cd
+  juris_Dscr dist_No off_S_H_Cd bal_Name bal_Num bal_Juris sup_Opp_Cd memo_Code
+  memo_RefNo bakRef_TID xRef_SchNm xRef_Match int_Rate int_CmteId]
+
   def initialize(body)
     @body = body
   end
@@ -19,6 +41,33 @@ class CalFileParser
       case row
       in ['SMRY', form_type, *cols]
         { 'line_Item' => cols[0], 'amount_A' => cols[1].to_f, 'form_Type' => form_type }
+      in ['CVR', form_type, *cols]
+        obj = CVR_HEADERS.zip(cols).to_h
+        obj
+      in ['S496', form_type, *cols] # S496 = Independent Expenditures Made
+        {
+          'form_Type' => form_type,
+          'tran_ID' => cols[0],
+          'tran_Amt1' => cols[1].to_f, # "Amount" in PDF
+          'exp_Date' => cols[2],
+          'date_Thru' => cols[3],
+          'tran_Dscr' => cols[4], # "expn_Desc" in PDF
+          'memo_Code' => cols[5],
+          'memo_RefNo' => cols[6],
+          # TODO: Add other fields here from the CVR:
+          # cand_NamL
+          # sup_Opp_Cd
+          # bal_Num
+        }
+      in ['RCPT', form_type, *cols]
+        # TODO: When Tran_Type = X, cols 18 and 19 have different headers.
+        obj = RCPT_HEADERS.zip(cols).to_h
+        obj['form_Type'] = form_type
+        obj
+        # TODO: Add other fields here from the CVR:
+        # cand_NamL
+        # sup_Opp_Cd
+        # bal_Num
       else
         # Row format not supported
       end
--- spec/lib/cal_file_parser_spec.rb
+++ spec/lib/cal_file_parser_spec.rb
@@ -16,4 +16,26 @@ RSpec.describe CalFileParser do
       expect(results).to include('form_Type' => 'E', 'line_Item' => '4', 'amount_A' => 23_697.00)
     end
   end
+
+  describe 'parsing a 496' do
+    let(:fixture) { File.read(Rails.root.join('spec', 'fixtures', 'cal', 'F496-212424488.txt')) }
+
+    it 'parses the cover sheet' do
+      results = parser.parse
+      expect(results).to include(hash_including(
+        'filer_NamL' => 'OAKLANDERS FOR COMMON SENSE, UNITED TO FIRE CARROLL FIFE AND ELECT WARREN LOGAN FOR CITY COUNCIL 2024'
+      ))
+    end
+
+    it 'parses S496 rows (independent expenditures made)' do
+      results = parser.parse
+      expect(results).to include(hash_including('form_Type' => 'F496', 'tran_Amt1' => 15000.00, 'tran_Dscr' => 'ELECTRONIC MEDIA ADS'))
+      expect(results).to include(hash_including('form_Type' => 'F496', 'tran_Amt1' => 15000.00, 'tran_Dscr' => 'ONLINE VIDEO ADS'))
+    end
+
+    it 'parses F496P3 rows (contributions >$100 received)' do
+      results = parser.parse
+      expect(results).to include(hash_including('form_Type' => 'F496', 'tran_Amt1' => 15000.00, 'tran_Dscr' => 'ONLINE VIDEO ADS'))
+    end
+  end
 end
