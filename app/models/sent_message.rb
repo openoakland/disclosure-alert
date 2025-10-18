925d2e0373ae1292d66c94ccc5386f3d658dcab2
Tom Dooner <tomdooner@gmail.com>
2025-10-18 13:43:33 -0700

    Loosen auto-unsubscribe logic
    We've had our power user get unsubscribed inadvertently a few times now,
    because her email provider occasionally returns a "permanent" failure
    that isn't actually permanent.
    
    To better handle this, let's add a configurable unsubscribe lookback, so
    we only unsubscribe users after receiving 2 "permanent" failures in the
    last month. I'll continue to loosen this threshold if we still get some
    accidental unsubscribes.

--- app/models/sent_message.rb
+++ app/models/sent_message.rb
@@ -2,4 +2,5 @@ class SentMessage < ApplicationRecord
   belongs_to :alert_subscriber, counter_cache: true
 
   scope :opened, -> { where.not(opened_at: nil) }
+  scope :bounced, -> { where.not(bounced_at: nil) }
 end
